7e66ac7d163dbe5c03dcc3dd3070e06a083e3073
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2022-10-04 16:16:19 -0700

    Create line_sweep.py

---
+++ line_sweep.py
@@ -0,0 +1,50 @@
+## 2406. Divide Intervals Into Minimum Number of Groups
+## Line Sweep is generally used when working with ranges and we want to find max overlap between all the intervals at any given point
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        
+        ## Simple line sweep algorithm
+        ## Time Complexity : O(nlogn)
+        ## Space Complexity : O(n)
+        
+        intervals = sorted(intervals, key = lambda x:x[0])
+        dicts = {}
+        
+        ## where an interval starts overlap+=1
+        ## where an interval ends overlap-=1
+        ## since [1,5] and [5,8] is an overlap
+        ## we add [1,4.99] and [5,7.99] to avoid this miscalculation
+        
+        ## max overlap at any given point will be the number of groups required
+        for x,y in intervals:
+            a = x - 0.01
+            if a in dicts:
+                dicts[a]+=1
+                
+            else:
+                dicts[a] = 1
+                
+            if y in dicts:
+                dicts[y]-=1
+            else:
+                dicts[y] = -1
+                
+            
+        t = []
+        
+        for x in dicts:
+            t.append([x,dicts[x]])
+            
+        t = sorted(t, key = lambda x:x[0])
+        
+        ans = 0
+        curr = 0
+        for x,y in t:
+            curr+=y
+            ans = max(ans, curr)
+            
+        return ans
+                
+            
+            
+        
