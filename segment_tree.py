118f103fd84353bbdebbe3587829558a3c56fbb9
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2023-03-29 13:07:51 -0700

    Update segment_tree.py

--- segment_tree.py
+++ segment_tree.py
@@ -11,12 +11,12 @@ def addRange(self, left: int, right: int) -> None:
         self.ranges[l:r] = sub_range
         print(self.ranges)
         
-    def queryRange(self, left: int, right: int) -> bool:
+def queryRange(self, left: int, right: int) -> bool:
         l = bisect.bisect_right(self.ranges, left)
         r = bisect.bisect_left(self.ranges, right)
         return l == r and l % 2 == 1
 
-    def removeRange(self, left: int, right: int) -> None:
+def removeRange(self, left: int, right: int) -> None:
         l = bisect.bisect_left(self.ranges, left)
         r = bisect.bisect_right(self.ranges, right)
         sub_range = []
@@ -24,5 +24,5 @@ def addRange(self, left: int, right: int) -> None:
             sub_range.append(left)
         if r % 2 == 1: # overlaps
             sub_range.append(right)
-            
+
         self.ranges[l:r] = sub_range
