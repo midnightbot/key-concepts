258fc0ea5c9dd65c2bdeec676cf62aff92a9ff11
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2023-04-28 10:38:09 -0700

    Create binary_search_templates.py

---
+++ binary_search_templates.py
@@ -0,0 +1,19 @@
+'''
+Template 1 
+Initial Condition: left = 0, right = length-1
+Termination: left > right
+Searching Left: right = mid-1
+Searching Right: left = mid+1
+
+Template 2
+Initial Condition: left = 0, right = length - 1
+Termination: left == right
+Searching Left: right = mid
+Searching Right: left = mid+1
+
+Template 3
+Initial Condition: left = 0, right = length-1
+Termination: left + 1 == right
+Searching Left: right = mid
+Searching Right: left = mid
+'''
